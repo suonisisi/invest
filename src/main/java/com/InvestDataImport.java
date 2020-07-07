e7c8712234b05c353e35970976d548f332b5b822
发芽的土豆 <953295434@qq.com>
2020-07-07 18:08:27 +0800

    20200707

---
+++ src/main/java/com/InvestDataImport.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: songkai
+ * @Date: 2020/7/7
+ */
+@SpringBootApplication
+public class InvestDataImport {
+    public static void main(String[] args){
+        SpringApplication.run(InvestDataImport.class,args);
+    }
+}
---
+++ src/main/java/com/invest/stock/basic/controller/BasicController.java
@@ -0,0 +1,19 @@
+package com.invest.stock.basic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author: songkai
+ * @Date: 2020/7/7
+ */
+
+@Controller
+public class BasicController {
+
+    @RequestMapping("stocklist")
+    public void StockList(String isHs,String status,String exchage){
+
+        char
+    }
+}
